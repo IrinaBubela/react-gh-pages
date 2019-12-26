8c6b76ae5a71c3c767e902911b320de90088db01
Irina <rishabubela@gmail.com>
2019-12-26 13:21:48 +0100

    Updates

---
+++ precache-manifest.07734d6f1bdeb842044eb7b0369a7ce9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d7eb914497ca130f59332e20c303d8f",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "bd90d79bfacb3da4616c",
+    "url": "/react-gh-pages/static/css/main.c4e5ab00.chunk.css"
+  },
+  {
+    "revision": "f26ad71c72f312ddac26",
+    "url": "/react-gh-pages/static/js/2.1b91ce91.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-gh-pages/static/js/2.1b91ce91.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bd90d79bfacb3da4616c",
+    "url": "/react-gh-pages/static/js/main.2a85d6c9.chunk.js"
+  },
+  {
+    "revision": "e1e91e16db01ad10fa13",
+    "url": "/react-gh-pages/static/js/runtime-main.81b86187.js"
+  },
+  {
+    "revision": "d26e29b3c237388f117384ae0322335a",
+    "url": "/react-gh-pages/static/media/bgd.d26e29b3.jpg"
+  },
+  {
+    "revision": "1679f504bfba988837c0c3360d8f17c9",
+    "url": "/react-gh-pages/static/media/weather.1679f504.jpg"
+  }
+]);
