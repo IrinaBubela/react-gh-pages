2704c12841fceb0be7003f0598970e45316265d6
Irina <rishabubela@gmail.com>
2019-12-26 19:39:25 +0100

    Final changes with input

--- src/components/form.js
+++ src/components/form.js
@@ -3,7 +3,7 @@ import React from "react";
 function Form(props) {
     return (
         <form onSubmit={props.weatherMethod}>
-            <input type="text" name="city" placeholder="Enter you city" />
+            <input type="text" name="city" placeholder="Enter city" />
             <button>Get a forecast</button>
         </form>
     )
